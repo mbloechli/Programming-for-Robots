bef9e339313074eddecad8bea588c0fe58a290e9
mbloechli <mbloechli@ethz.ch>
2023-02-23 16:24:35 +0100

    added a subscriber for the pointcloud, print the number of point in the pointcloud to terminal. This solves optional task 11

--- src/smb_highlevel_controller/include/smb_highlevel_controller/SmbHighlevelController.hpp
+++ src/smb_highlevel_controller/include/smb_highlevel_controller/SmbHighlevelController.hpp
@@ -2,6 +2,7 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
 
 namespace smb_highlevel_controller {
 
@@ -23,9 +24,11 @@ public:
 
 private:
 	ros::NodeHandle nodeHandle_;
-	ros::Subscriber subscriber_;
+	ros::Subscriber subscriber_laser_;
+	ros::Subscriber subscriber_pointcloud_;
 
-	void topicCallback(const sensor_msgs::LaserScan data);
+	void topic_laser_Callback(const sensor_msgs::LaserScan data);
+	void topic_pointcloud_Callback(const sensor_msgs::PointCloud2 data);
 };
 
 } /* namespace */
